740f84a787702cabb718cf58db03f2adbca45b63
Mohammed-Azhan <azhanpalli@gmail.com>
2025-05-28 17:04:38 +0530

    updates

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+function App() {
+
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image.png" alt="" /></SwiperSlide>
+
+      </Swiper>
+    </>
+  )
+}
+
+export default App
